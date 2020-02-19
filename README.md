954b27ae152a73607038ff767c5d713c9a86f657
ICEY <icey@icey.tech>
2020-02-20 03:13:35 +0800

    飞天猴巧夺宝盒

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# project-dag
-一个为文本 RPG 游戏设计的 DSL，主要用于和 xyy 的 [GalTextEngine](https://github.com/HoshinoTented/GalTextEngine) 进行“军备竞赛”，不过其本身的完成度远超预期。
+# Project-DAG
+一个为文本 RPG 游戏设计的 DSL，主要用于和 xyy 的 [GalTextEngine](https://github.com/HoshinoTented/GalTextEngine) 进行“军备竞赛”，不过目前来看，这个项目的完成度远超预期。
+
+“DAG”的含义是有向无环图，一定程度上反映的是项目的设计思想。
 
 ## 设计思想
 * 一个 RPG 游戏由若干个场景 `scene` 组成
@@ -9,3 +11,10 @@
   * 为了方便话痨游戏，同时减少游戏的状态总数， `desc` 可以是嵌套了任意层的 `list`，参见 `big-gossip.rkt`
 * 选择一个 `choice` 会让游戏移动到下一个 `scene`
   * 同理，移动到哪一个 `scene` 也可以由一个 `lambda` 表达式决定，同样参见 `game-with-env.rkt`
+
+## 使用方法
+只需要 
+```racket
+(require "dagc.rkt")
+```
+就可以使用全部的功能了。建议先阅读示例。
--- game-with-env.rkt
+++ game-with-env.rkt
@@ -9,15 +9,22 @@
              '("朴实" "无华" "枯燥" "副作用")
              "这个游戏，就是这么地有副作用，且枯燥"
              (scenes
-               (scene 1 "周一请闭眼，朱一旦请睁眼，今天你要开除员工吗？"
-                        (choices (choice "开除" 
+               (scene 1 "无聊的周一，朱一旦请睁眼，今天你要开除员工吗？"
+                        (choices (choice "开除"
+                                         ; 这里不再使用一个常量来决定移动到哪一个场景
+                                         ; 而是使用一个 lambda，根据游戏环境 game-env 中的 'cnt
+                                         ; 确定移动到那个状态
                                          (lambda (game-env) (if (= (game-env 'get 'cnt) 3)
+                                                                ; 如果已经开除了三个员工，就此收手
+                                                                ; 否则继续开除
                                                                 3
                                                                 2)))
                                  (choice "不开除" 4)))
                (scene 2
+                 ; 这里也不再使用常量作为 desc，而是根据 game-env 中的 'cnt 更新desc
                  (lambda (game-env)
                    (begin
+                     ; 此外每次进入场景 2，也就意味着又开除了一个员工，'cnt 自增 1
                      (game-env 'set 'cnt (+ (game-env 'get 'cnt) 1))
                      (string-append "无聊的周一，翻开我名下的杂志，随便指了一名十佳员工，"
                                     "吩咐人事经理把他辞退。\n"
@@ -25,4 +32,6 @@
                  (choices (choice "继续" 1)))
                (scene 3 "辞退的够多了，再这样下去，公司迟早要完旦"  game-over)
                (scene 4 "有钱人的快乐，往往就是这么朴实无华，且枯燥" game-over))
+             ; 初始化动作，设置游戏环境中 'cnt 初始为 0
+             ; 初始化动作可以有多个，从上向下依次执行
              (lambda (game-env) (game-env 'set 'cnt 0))))
