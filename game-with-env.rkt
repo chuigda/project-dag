71a714ed34e70a5bb5c8781c1b71d68d4d6733ce
htydog <htydog@outlook.com>
2020-02-19 17:34:06 +0800

    我没有说过这些话------朱一旦

--- game-with-env.rkt
+++ game-with-env.rkt
@@ -19,9 +19,9 @@
                  (lambda (game-env)
                    (begin
                      (game-env 'set 'cnt (+ (game-env 'get 'cnt) 1))
-                     (string-append "无聊的周一，随便挑了一名十佳员工，吩咐人事将他开除。"
+                     (string-append "无聊的周一，翻开我名下的杂志,随便指了一名十佳员工，吩咐人事经理把他辞退。"
                                     "你已经开除了 " (~a (game-env 'get 'cnt)) " 个员工。")))
                  (choices (choice "继续" 1)))
-               (scene 3 "开除的够多了，再这样下去，公司迟早要完旦" game-over)
-               (scene 4 "有钱的人生，就是这样朴实无华，且枯燥" game-over))
+               (scene 3 "辞退的够多了，再这样下去，公司迟早要完旦"  game-over)
+               (scene 4 "有钱人的快乐，往往就是这么朴实无华，且枯燥" game-over))
              (lambda (game-env) (game-env 'set 'cnt 0))))
